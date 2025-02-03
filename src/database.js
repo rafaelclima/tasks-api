40e541773546a57046cb11780969999af35c871a
Rafael Lima <rafaelclima.ti@gmail.com>
2025-02-03 17:14:26 -0300

    :lipstick: feat: alterando e melhorando as respostas da api

--- src/database.js
+++ src/database.js
@@ -1,74 +1,81 @@
-import { URL } from 'node:url'
-import fs from 'node:fs/promises'
+import { URL } from 'node:url';
+import fs from 'node:fs/promises';
 
-const databasePath = new URL('db.json', import.meta.url)
+const databasePath = new URL('db.json', import.meta.url);
 
 export class Database {
-  #database = {}
+  #database = {};
 
   constructor() {
     fs.readFile(databasePath, 'utf8')
       .then((database) => {
-        this.#database = JSON.parse(database)
+        this.#database = JSON.parse(database);
       })
       .catch(() => {
-        this.#persist()
-      })
+        this.#persist();
+      });
   }
 
   #persist() {
-    fs.writeFile(databasePath, JSON.stringify(this.#database))
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
-  select(table, search) {
-    let data = this.#database[table] || []
+  select(table, id, search) {
+    let data = this.#database[table] || [];
 
     if (search) {
       data = data.filter((row) => {
         return Object.entries(search).some(([key, value]) => {
-          return row[key].toLowerCase().includes(value.toLowerCase())
-        })
-      })
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
     }
 
-    return data
+    if (id) {
+      const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+      if (rowIndex > -1) {
+        return data;
+      }
+    }
+
+    //return data;
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data)
+      this.#database[table].push(data);
     } else {
-      this.#database[table] = [data]
+      this.#database[table] = [data];
     }
 
-    this.#persist()
-    return data
+    this.#persist();
+    return data;
   }
 
   update(table, id, data) {
-    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
-      this.#database[table][rowIndex] = { id, ...data }
-      this.#persist()
+      this.#database[table][rowIndex] = { id, ...data };
+      this.#persist();
     }
   }
 
   path(table, id, data) {
-    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
-      this.#database[table][rowIndex] = { ...this.#database[table][rowIndex], ...data }
-      this.#persist()
+      this.#database[table][rowIndex] = { ...this.#database[table][rowIndex], ...data };
+      this.#persist();
     }
   }
 
   delete(table, id) {
-    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
-      this.#database[table].splice(rowIndex, 1)
-      this.#persist()
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
     }
   }
 }
